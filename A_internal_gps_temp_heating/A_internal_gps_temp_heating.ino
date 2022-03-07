47fb3c36e44b6286be7d7c6a3b036a69d95a83fb
Andromeda Kepecs <amkepecs@gmail.com>
2022-03-06 18:16:27 -0800

    working internal temp reading and rtc. using DS3234 module and library. rtc has been configured

--- A_internal_gps_temp_heating/A_internal_gps_temp_heating.ino
+++ A_internal_gps_temp_heating/A_internal_gps_temp_heating.ino
@@ -1,5 +1,8 @@
-#include <SparkFunDS1307RTC.h>
-#include <Wire.h>
+#include <SparkFunDS3234RTC.h>
+#include <SPI.h>
+
+#define DS13074_CS_PIN 10
+
 #include <Wire.h> //Needed for I2C to GNSS
 
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h> //http://librarymanager/All#SparkFun_u-blox_GNSS
@@ -18,10 +21,11 @@ By USS Urmom
 
 // Digital output pins
 const int HEAT_PAD = 10;
-// RTC uses arbitrary digital pin
+const int RTC = 10;
+// RTC and gps use additional pins
 
 // Analog input pins
-const int THERM_PIN = 0;
+const int THERM_PIN = 0; // Connect other end of thermistor to 5V
 
 // Temps to trigger heating pad in Celcius
 const int MIN_TEMP = 10;
@@ -36,21 +40,23 @@ const float SH_C = 0.000000085663516;
 const float R1 = 10000;
 
 // Timing
-const int DELAY = 10; // How many seconds the sensor takes a reading
+const int DELAY = 5; // How many seconds the sensor takes a reading
 const int SEC = 1000; // 1 sec = 1000 ms
 const int BAUD_RATE = 9600;
 
 void setup() {
-  rtc.begin();
-  rtc.autotime(); // May need to be manually changed to launch time
+  rtc.begin(RTC);
+  Serial.begin(BAUD_RATE);
+
+  // RTC has been configured, behind by approx 10 sec
+  //rtc.setTime(0, 12, 18, 1, 6, 3, 22);
+  //rtc.autoTime(); 
 
   pinMode(HEAT_PAD, OUTPUT);
 
-  Serial.begin(BAUD_RATE);
+  //Wire.begin();
 
-  Wire.begin();
-
-  if (myGNSS.begin() == false) // Connect to the u-blox module using Wire port
+  /*if (myGNSS.begin() == false) // Connect to the u-blox module using Wire port
   {
     Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
     while (1);
@@ -58,6 +64,10 @@ void setup() {
 
   myGNSS.setI2COutput(COM_TYPE_UBX); // Set the I2C port to output UBX only (turn off NMEA noise)
   myGNSS.saveConfigSelective(VAL_CFG_SUBSEC_IOPORT); // Save (only) the communications port settings to flash and BBR
+  */
+
+  // csv file headers
+  Serial.println("Hour,Minute,Second,Temperature(C),Heatpad_on?,Latitude,Longitude,Altitude");
 }
 
 void loop() {
@@ -66,15 +76,15 @@ void loop() {
   // Read time:
   int second = rtc.second();
   int minute = rtc.minute();
-  int hour = rtc.hour()
+  int hour = rtc.hour();
 
   // Read GPS data
   // Query module only every second. Doing it more often will just cause I2C traffic.
   // The module only responds when a new position is available
-  long latitude = myGNSS.getLatitude();
+  /*long latitude = myGNSS.getLatitude();
   long longitude = myGNSS.getLongitude();
   long altitude = myGNSS.getAltitude();
-  byte SIV = myGNSS.getSIV();
+  byte SIV = myGNSS.getSIV();*/
 
   // Read temperature
   int Vo = analogRead(THERM_PIN); // Read in voltage from thermistor pin
@@ -82,18 +92,34 @@ void loop() {
   int temperature = int(calculate_temperature(R2));  
 
   // Trigger heating pad if below min temperature
-  heatpad_on = false
+  bool heatpad_on = false;
   if (temperature < MIN_TEMP) {
     digitalWrite(HEAT_PAD, HIGH);
-    heatpad_on = true
+    heatpad_on = true;
   }
   else if (temperature > MAX_TEMP) {
     digitalWrite(HEAT_PAD, LOW);
-    heatpad_on = false
+    heatpad_on = false;
   }
 
   // Print all data in csv format
-  print("Time,Temperature(C),Heatpad_on?,Latitude,Longitude,Altitude")
+  Serial.print(hour);
+  Serial.print(",");
+  Serial.print(minute);
+  Serial.print(",");
+  Serial.print(second);
+  Serial.print(",");
+  Serial.print(temperature);
+  Serial.print(",");
+  Serial.print(heatpad_on);
+  Serial.print(",");
+  Serial.println();
+  /*
+  Serial.print(latitude);
+  Serial.print(",");
+  Serial.print(longitude);
+  Serial.print(",");
+  Serial.print(altitude);*/
 
   delay(DELAY * SEC); // Delay in seconds between updates
 }
